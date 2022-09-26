5b6ddc9b0422bd61a2d9555ab49ec4aafbd67e4c
markusss23 <tremelmarek@gmail.com>
2022-09-26 19:43:13 +0200

    zakomentování route -> device/type ... čekání na opravu

--- WebApplication/app.py
+++ WebApplication/app.py
@@ -23,12 +23,12 @@ def action(device):
     conn = get_db_connection()
     datas = conn.execute("select  device.label as 'device', scene.label as 'scene' from device, scene inner join scene_device sd on device.id = sd.device_id inner join scene s on s.id = sd.scene_id where scene.label = ?", (device, )).fetchall()
     return render_template('device.html', datas=datas)
-
+"""""
 @app.route("/<device>/<type>")
 def action(type):
     conn = get_db_connection()
     datas = conn.execute("select  device.label as 'device', scene.label as 'scene' from device, scene inner join scene_device sd on device.id = sd.device_id inner join scene s on s.id = sd.scene_id where scene.label = ?", (type, )).fetchall()
     return render_template('device.html', datas=datas)
-
+"""
 if __name__ == '__main__':
     app.run()
