7c9c7454039edc6cc731c50915ecce6039f3bf7f
markusss23 <tremelmarek@gmail.com>
2022-09-29 18:51:59 +0200

    routování vyřešeno, je třeba dodělat crud funkce, třeba myslet na broker, čtení hodnot podívat se proč nejde 'try' a 'except' vždy spadne stránka s errorem ... některé vyřešené,
    potřeba zapracovat na designu

--- WebApplication/app.py
+++ WebApplication/app.py
@@ -1,17 +1,20 @@
 import paho.mqtt.client as mqtt
 import sqlite3
+
+from _testcapi import awaitType
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
+"""""
 mqttc = mqtt.Client()
 mqttc.connect("192.168.0.15", 1883, 60)
 mqttc.loop_start()
-
+"""
 """
 je potřeba dodělat CRUD funkce,
 
 """
+
 
 def get_db_connection():
     ## dodělat ošetření
@@ -46,7 +49,7 @@ def scene(scene):
         conn = get_db_connection()
         datas = conn.execute(
             "select d.label, d.device_topic, d.is_active, d.identifier FROM device d JOIN scene_device sd ON d.id = sd.device_id JOIN scene s ON s.id = sd.scene_id WHERE s.scene_topic = ?",
-            (scene+"/",)).fetchall()
+            (scene + "/",)).fetchall()
         template_data['datas'] = datas
     except:
         pass
@@ -54,24 +57,45 @@ def scene(scene):
     return render_template('scene.html', **template_data)
 
 
-@app.route("/<scene>/<device>/<action>")
-def device(scene, device, action):
-    conn = get_db_connection()
-    rest_topic = conn.execute("select identifier from device where device.device_topic = ?", (device+"/",)).fetchall()
-    end_topic = ""
-    for topic in rest_topic:
-        end_topic = ' '.join(topic)
+@app.route("/<scene>/<device>/")
+def device(scene, device):
+    template_data = {
+        'datas': False,
+        'label': device
+    }
     try:
+        conn = get_db_connection()
+        datas = conn.execute("select * from device as d where d.device_topic = ?",
+                             (device + "/", )).fetchall()
+        template_data['datas'] = datas
+    except:
+        pass
+    return render_template('device.html', **template_data)
 
+
+@app.route("/<scene>/<device>/<action>")
+def action(scene, device, action):
+    print(type(action))
+    conn = get_db_connection()
+    try:
+        rest_topic = conn.execute("select identifier from device where device.device_topic = ?",
+                                  (device + "/",)).fetchall()
+        end_topic = ""
+        for topic in rest_topic:
+            end_topic = ' '.join(topic)
+    except:
+        pass
+    try:
+        """""
         if action == "1":
             mqttc.publish(scene+"/"+device+"/"+end_topic + "command", "on")
         if action == "0":
             mqttc.publish(scene+"/"+device+"/"+end_topic + "command", "off")
-
-        return scene+"/"+device+"/"+end_topic + "command"
+        """
+        return scene + "/" + device + "/" + end_topic + "command"
 
     except:
-        return "error"
+        pass
 
 
 if __name__ == '__main__':
