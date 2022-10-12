0dc099562276a58a61e021ece0ad2905cb7180d1
markusss23 <tremelmarek@gmail.com>
2022-10-12 14:28:16 +0200

    vytvořen login system, ověřovánbí supervisor, dodělat ošetření id > 0, přetypovat, začít dělat na designu, prvně inputy dodělat editaci a mazání,
    //////////////
    třeba myslet na broker, čtení hodnot
    potřeba zapracovat na designu

--- WebApplication/app.py
+++ WebApplication/app.py
@@ -1,10 +1,20 @@
+from werkzeug.security import generate_password_hash
+
 from views import views
 
 import sqlite3
 
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, render_template, request, flash, redirect, session
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
+
+
+@app.before_request
+def load_logged_in_user():
+    views.load_logged_in_user()
+
+
 """""
 mqttc = mqtt.Client()
 mqttc.connect("192.168.0.15", 1883, 60)
@@ -23,162 +33,15 @@ def get_db_connection():
     return conn
 
 
-@app.route("/")
-def main():
-    template_data = {
-        'scenes': False,
-    }
-    try:
-        conn = get_db_connection()
-        scenes = conn.execute("select * from scene").fetchall()
-        conn.close()
-        template_data['scenes'] = scenes
-    except:
-        pass
-
-    return render_template('index.html', **template_data)
-
-
 @app.route("/add/")
 def add():
     return "nabídka přidání věcí"
 
 
-@app.route("/types/add/", methods=('POST', 'GET'))
-def add_typ():
-    try:
-        if request.method == 'POST':
-            label = request.form['label']
-            typ = request.form['typ']
-            jednotka = request.form['jednotka']
-            minimum = request.form['min']
-            maximum = request.form['max']
-            interval = request.form['interval']
-            type_topic = request.form['type_topic']
-            is_active = request.form.get('is_active')
 
-            conn = get_db_connection()
-            if is_active is None:
-                is_active = 0
-            else:
-                is_active = 1
-            if type_topic[-1] != "/":
-                type_topic = type_topic + "/"
-            error = None
-
-            if not label:
-                error = 'Název scény chybí'
-            if not typ:
-                error = 'Chybí typ'
-            if not type_topic:
-                error = 'Adresa chybí'
-
-            if error is None:
-                try:
-                    conn.execute("INSERT INTO type VALUES (NULL,?,?,?,?,?,?,?,?)",
-                                 (label, typ, jednotka, minimum, maximum, interval, type_topic, is_active))
-                    conn.commit()
-                except conn.IntegrityError:
-                    error = f" Tato adresa již existuje "
-                else:
-                    return redirect('/')
-            flash(error)
-    except:
-        pass
-
-    return render_template('type/typeAdd.html')
 
 
 # -----------------------------------
-
-
-@app.route("/devices/add/", methods=('POST', 'GET'))
-def add_device():
-    template_data = {
-        'types': False,
-    }
-    conn = get_db_connection()
-
-    types = conn.execute("select * from type")
-    template_data['types'] = types
-
-    try:
-        if request.method == 'POST':
-            label = request.form['label']
-            device_topic = request.form['device_topic']
-            is_active = request.form.get('is_active')
-            select = request.form.get('types')
-            pin = request.form['pin']
-
-            if is_active is None:
-                is_active = 0
-            else:
-                is_active = 1
-            if device_topic[-1] != "/":
-                device_topic = device_topic + "/"
-            error = None
-
-            if not label:
-                error = 'Název scény chybí'
-            if not device_topic:
-                error = 'Adresa chybí'
-            if not pin:
-                error = 'Pin chybí'
-
-            if error is None:
-                try:
-                    conn.execute("INSERT INTO device VALUES (NULL,?,?,?,?,?)",
-                                 (select, label, device_topic, is_active, pin + '/'))
-                    conn.commit()
-                except conn.IntegrityError:
-                    error = f" Tato adresa již existuje "
-                else:
-                    return redirect('/')
-            flash(error)
-    except:
-        pass
-
-    return render_template('device/deviceAdd.html', **template_data)
-
-
-@app.route("/devices/")
-def devices():
-    template_data = {
-        'devices': False,
-        'label': 'Výpis všech zařízení'
-    }
-    try:
-        conn = get_db_connection()
-        devices = conn.execute("select * from device").fetchall()
-        conn.close()
-        template_data['devices'] = devices
-    except:
-        pass
-    return render_template('device/deviceList.html', **template_data)
-
-
-@app.route("/devices/<int:id>/")
-def device(id):
-    template_data = {
-        'devices': False,
-        'scenes': False,
-    }
-    try:
-        conn = get_db_connection()
-        devices = conn.execute(
-            "select * from device where device.id = ?",
-            (id,)).fetchall()
-
-        scenes = conn.execute(
-            "select s.id, s.label, s.scene_topic, s.is_active from scene s join scene_device sd on s.id = sd.scene_id join device d on d.id = sd.device_id where d.id=?;",
-            (id,)).fetchall()
-
-        template_data['devices'] = devices
-        template_data['scenes'] = scenes
-    except:
-        pass
-
-    return render_template('device/deviceDetail.html', **template_data)
 
 
 @app.route("/<scene>/<device>/<action>")
@@ -213,10 +76,32 @@ def action(scene, device, action):
         pass
 
 
+app.add_url_rule('/', view_func=views.main)
+
+# --------------
+
+app.add_url_rule('/user/add/', view_func=views.user_add, methods=("POST", "GET"))
+app.add_url_rule('/user/login/', view_func=views.user_login, methods=("POST", "GET"))
+app.add_url_rule('/user/logout/', view_func=views.user_logout)
+
+# -------------
+
 app.add_url_rule('/scenes/', view_func=views.scenes)
 app.add_url_rule('/scenes/<int:id>/', view_func=views.scene)
-app.add_url_rule('/scenes/<int:id>/edit/', view_func=views.edit_scene)
-app.add_url_rule('/scenes/add/', view_func=views.add_scene, methods=("POST", "GET"))
+app.add_url_rule('/scenes/<int:id>/edit/', view_func=views.scenes_edit)
+app.add_url_rule('/scenes/add/', view_func=views.scenes_add, methods=("POST", "GET"))
+
+# -------------
+
+app.add_url_rule('/devices/', view_func=views.devices)
+app.add_url_rule('/devices/<int:id>/', view_func=views.device)
+app.add_url_rule('/devices/add/', view_func=views.devices_add, methods=("POST", "GET"))
+
+# ---------------
+
+app.add_url_rule('/types/', view_func=views.types)
+app.add_url_rule('/types/<int:id>/', view_func=views.type)
+app.add_url_rule('/types/add/', view_func=views.types_add, methods=("POST", "GET"))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
