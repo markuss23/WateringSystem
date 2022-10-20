5481f5dba926ce132db1aaefe744734b021214ab
markusss23 <tremelmarek@gmail.com>
2022-10-20 17:46:18 +0200

    začlo se dělat na designu, porovnávání bootstrapu a braní, co potřebuji začít dělat na designu, prvně inputy -> importovaná knihovna, zatím nefunkční dodělat editaci a mazání,
    úprava homepage
    
    //////////////
    třeba myslet na broker, čtení hodnot
    potřeba zapracovat na designu

--- WebApplication/views/views.py
+++ WebApplication/views/views.py
@@ -126,12 +126,15 @@ def load_logged_in_user():
 def main():
     template_data = {
         'scenes': False,
+        'devices': False,
     }
     try:
         conn = get_db_connection()
-        scenes = conn.execute("select * from scene").fetchall()
+        scenes = conn.execute("select * from scene where is_active=1").fetchall()
+        devices = conn.execute("select * from device where is_active=1").fetchall()
         conn.close()
         template_data['scenes'] = scenes
+        template_data['devices'] = devices
     except:
         pass
 
