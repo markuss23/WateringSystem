40722d889aa5d60c26c32f4d76aca60cc794d007
markusss23 <tremelmarek@gmail.com>
2022-12-16 19:50:30 +0100

    Dodělávky jak pracovat s delete??? vyřešit dodělat mazání, připojování ... scén a zařízení /////////////// snad dodělaný design :)

--- WebApplication/static/js/mqtt-socket.js
+++ WebApplication/static/js/mqtt-socket.js
@@ -7,6 +7,7 @@ let payload = [];
 let text;
 let textPayload;
 myStorage = window.localStorage;
+let clickable = false;
 
 
 function removeDuplicates() {
@@ -27,6 +28,32 @@ $(document).ready(function () {
     let payloads = [];
     topics.push(JSON.parse(localStorage.getItem('topic')));
     payloads.push(JSON.parse(localStorage.getItem('payload')));
+    console.log($('#typ').text().toString());
+
+    if ($('#typ').text() == "Input") {
+        $('#publish').addClass("bg-components-dark");
+        $('#publish').removeClass("bg-components, link-brown");
+    } else {
+        $('#publish').click(function (event) {
+
+            let topics = $('#topic').text();
+            if (getShelly(topics) === true) {
+                topics = topics + '/command';
+            }
+            let message = $('#publish').text();
+            let qos = 2;
+            let data = '{"topic": "' + topics + '", "message": "' + message + '", "qos": ' + qos + '}';
+            if ($('#publish').text() === 'on') {
+                $('#publish').text('off');
+                $('#publish').val('off');
+            } else {
+                $('#publish').text('on');
+                $('#publish').val('on');
+            }
+
+            socket.emit('publish', data = data);
+        });
+    }
     for (let i = 0; i < topics[0].length; i++) {
         if ($('#topic').text() === topics[0][i]) {
             if (parseFloat(payloads[0][i])) {
@@ -35,6 +62,10 @@ $(document).ready(function () {
             } else {
                 $('#publish').text(changeState(payloads[0][i]));
             }
+        } else {
+            $('#publish').text("Nefunkční");
+            $('#publish').addClass("bg-components-dark");
+            $('#publish').removeClass("bg-components, link-brown");
         }
     }
 
@@ -55,7 +86,7 @@ socket.on('mqtt_message', function (data) {
 
     payload[topic.indexOf(data['topic'])] = data['payload'];
     for (let i = 0; i < payload.length; i++) {
-        if (!payload[i]){
+        if (!payload[i]) {
             payload[i] = textPayload[i];
         }
     }
@@ -70,25 +101,6 @@ socket.on('mqtt_message', function (data) {
     }
 })
 
-$('#publish').click(function (event) {
-
-    let topics = $('#topic').text();
-    if (getShelly(topics) === true) {
-        topics = topics + '/command';
-    }
-    let message = $('#publish').text();
-    let qos = 2;
-    let data = '{"topic": "' + topics + '", "message": "' + message + '", "qos": ' + qos + '}';
-    if ($('#publish').text() === 'on') {
-        $('#publish').text('off');
-        $('#publish').val('off');
-    } else {
-        $('#publish').text('on');
-        $('#publish').val('on');
-    }
-
-    socket.emit('publish', data = data);
-});
 
 function changeState(state) {
     if (state === 'on') {
